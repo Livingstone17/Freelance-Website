25e7e74e59701ce2bee35c84da95a84d038e75d8
Samson Peter Omobolaji <32388423+Livingstone17@users.noreply.github.com>
2019-09-19 23:26:40 -0700

    My fourth commit
    90% done

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function getAll(){
     content +=
     `
         <div class="row">    
-        <div class="col-md-4 col-lg-4 col-sm-12" id="">
+        <div class="col-md-4 col-lg-4 col-sm-12" id="" onclick="openLancers(${e[i].id})">
         <div class="card" >
         <div class="card-img">
             <img src="images/team3.jpg" class="img-fluid">
@@ -104,7 +104,7 @@ function getAll(){
                 
              content +=
                     `
-                    <div class="col-md-4 col-lg-4 col-sm-12" id="">
+                    <div class="col-md-4 col-lg-4 col-sm-12" id="" onclick="openLancers(${e[i].id})">
                     <div class="card" >
                     <div class="card-img">
                         <img src="images/team3.jpg" class="img-fluid">
@@ -129,7 +129,7 @@ function getAll(){
                 
            content +=
                         `   
-                        <div class="col-md-4 col-lg-4 col-sm-12" id="">
+                        <div class="col-md-4  col-lg-4 col-sm-12" id="" onclick="openLancers(${e[i].id})">
                         <div class="card" >
                         <div class="card-img">
                             <img src="images/team3.jpg" class="img-fluid">
@@ -201,29 +201,33 @@ $(document).ready(function(){
    });
 
    // //   delete a freelancer
- $('#deleteUser').on('click',(e) =>{
-    e.preventDefault();
-let id = e.target.id.split('del-')[0];
+ function deleter() {
+    // e.preventDefault();
+    const id = window.location.toString().split('?')[1];
+
+// let id = e.target.id.split('del-')[0];
 alert (id);
 $.ajax({
     url:`http://localhost:3000/Freelancers/${id}`,
     method: 'delete'
 }).done((e)=> {
     alert('deleted');
+
 });
 
-
+ }
 //  to edit users profile
 
-$('#updateProfile').click((e)=> {
-    console.log('submit form success')
-    e.preventDefault();
+function updater(){
+    // alert('submit form success');
+    const id = window.location.toString().split('?')[1];
+    // e.preventDefault();
     var name = $('#myName').val();
     var mail = $('#myEmail').val();
-    var firstname = $('#fName').val();
-    var lastname = $('#lName').val();
+    var firstname = $('#firstname').val();
+    var lastname = $('#lastname').val();
     var specialty = $('#mySpec').val();
-    var phonenumber = $('#pNumber').val();
+    var phonenumber = $('#phonenumber').val();
     var password = $('#password1').val();
     var minimumwage = $('#minimumWage').val();
     var bio = $('#bio').val();
@@ -238,34 +242,7 @@ $('#updateProfile').click((e)=> {
         }
     }).done((e)=> {
         alert('done');
-        $('#freelancers').append(
-            `<h4>
-                ${e.name}
-             </h4>
-             <p>
-             ${e.mail}
-             </p>
-             <p>
-             ${e.fName}
-             </p>
-             <p>
-             ${e.lName}
-             </p>
-             <p>
-             ${e.minimumWage}
-             </p>
-             <p>
-             ${e.bio}
-            <p>
-            ${e.address}
-            </p>
-            <h4>
-              ${e.spec}  
-            </h4>
-            <h5>
-            ${e.rating}
-            </h5>`
-        );
+        getfreelancer();
         $('#firstname').val('');
         $('#lastname').val('');
         $('#minimumwage').val('');
@@ -274,106 +251,34 @@ $('#updateProfile').click((e)=> {
         $('#phonenumber').val('');
     });
 // }); 
-});
-function getOne(){
+};
+
+function getOne(id){
 
 return $.ajax({
         url: `http://localhost:3000/Freelancers/${id}`,
         method: 'get',
     }).done((e)=> {
-    //  alert('done');
-    let content = '';
-     for(var i=0; i< e.length; i++){
-//          var container = document.getElementById("container");
-//    container.innerHTML += '<div class="box"></div>';
-if(i === 0){
-    console.log(e[1], 'first');
-    
-    content +=
-    `
-        <div class="row">    
-        <div class="col-md-4 col-lg-4 col-sm-12" id="">
-        <div class="card" >
-        <div class="card-img">
-           <a href="freeLancersProfile.html"><image src="team2.jpg"></a>
-        </div>
-    <div class="card-body">
-        <h4 class="card-title">${e[i].name}</h4>
-        <p class="card-text">${e[i].specialty}</p>
-        <p> ${e[i].email}</p>
-        <p>${e[i].firstname}</p>
-        <p>${e[i].lastname}</p>
-        <p>${e[i].minimumwage}</p>
-        <p>${e[i].phonenumber}</p>
-        <p>${e[i].bio}</p>
-    </div>
-    <div class="card-footer">
-        <p>${e[i].address}</p>
-    </div>
-    </div>
-</div>`;
-}else if((i+1)%3){
-                console.log((i+1)%3, 'not by 3');
-                
-             content +=
-                    `
-                    <div class="col-md-4 col-lg-4 col-sm-12" id="">
-                    <div class="card" >
-                    <div class="card-img">
-                        <img src="images/team3.jpg" class="img-fluid">
-                    </div>
-                <div class="card-body">
-                    <h4 class="card-title">${e[i].name}</h4>
-                    <p class="card-text">${e[i].specialty}</p>
-                    <p> ${e[i].mail}</p>
-                    <p>${e[i].firstname}</p>
-                    <p>${e[i].lastname}</p>
-                     <p>${e[i].minimumwage}</p>
-                     <p>${e[i].phonenumber}</p>
-                      <p>${e[i].bio}</p>
-                </div>
-                <div class="card-footer">
-                    <p>${e[i].address}</p>
-                </div>
-                </div>
-            </div>`;
-            } else {
-
-                
-           content +=
-                        `   
-                        <div class="col-md-4 col-lg-4 col-sm-12" id="">
-                        <div class="card" >
-                        <div class="card-img">
-                            <img src="images/team3.jpg" class="img-fluid">
-                        </div>
-                    <div class="card-body">
-                        <h4 class="card-title">${e[i].name}</h4>
-                        <p class="card-text">${e[i].specialty}</p>
-                        <p> ${e[i].mail}</p>
-                        <p>${e[i].firstname}</p>
-                        <p>${e[i].lastname}</p>
-                        <p>${e[i].minimumwage}</p>
-                        <p>${e[i].phonenumber}</p>
-                        <p>${e[i].bio}</p>
-                    </div>
-                    <div class="card-footer">
-                        <p>${e[i].address}</p>
-                    </div>
-                    </div>
-                </div>
-                </div>
-                        <div class="row"> 
-                    `;
-                    
-                }
-            
-     }
-
-     $('#freelancers').append(content);     
-    
+            return e;
     });
 
 
 }
- })
+ 
+
+ function openLancers(id) {
+//    alert(window.location);
+   window.location = `freeLancersProfile.html?${id}`;
+ }
+
+async function getfreelancer(){
+    const id = window.location.toString().split('?')[1];
+    const user = await getOne(id);
+    let profile = '';
+    profile += `<div> <b>Name:</b> <p> ${user.firstname} ${user.lastname}</p></div>`;
+    profile += `<div> <b>Specialty:</b> <p> ${user.specialty}</p>
+    <b>Email:</b><p>${user.email}</p>
+    <b>Phone Number:</b><p>${user.phonenumber}</p>`;
+    $('#yourProfile').empty();
+    $('#yourProfile').append(profile)
+}
